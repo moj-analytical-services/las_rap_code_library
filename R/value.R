13fb5c11faf19e669307a9dc606b3f382b4dd7d6
mark edwardes <mark.edwardes1@justice.gov.uk>
2018-04-04 10:23:53 +0000

    rmeoved string quotes in sum formula

--- R/value.R
+++ R/value.R
@@ -19,13 +19,13 @@ value <- function (tibble, reference_date, volval) {
   tibble2 <- dplyr::summarize(group, volval = sum(volval))
 
   if      (reference_date == "current")    {
-    sum(tibble2[tibble2 == quarter1, "volval"])
+    sum(tibble2[tibble2 == quarter1, volval])
     }
   else if (reference_date == "previous_q") {
-    sum(tibble2[tibble2 == quarter2, "volval"])
+    sum(tibble2[tibble2 == quarter2, volval])
     }
   else if (reference_date == "previous_y") {
-    sum(tibble2[tibble2 == quarter3, "volval"])
+    sum(tibble2[tibble2 == quarter3, volval])
     }
   else    print(
     "Please enter a reference_date as 'current', previous_q' or 'previous_y'")
