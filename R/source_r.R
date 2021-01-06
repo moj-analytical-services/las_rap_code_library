74a25739591267cf1817de1c07bac1a0fa9ba93c
markrangan <mark.ranganathan@justice.gov.uk>
2021-01-06 16:19:10 +0000

    Expanded documentation of source_r function

--- R/source_r.R
+++ R/source_r.R
@@ -1,6 +1,9 @@
 #' Choose between automatic or manual source script.
 #'
 #' @description You can choose between automatic or manual source script. The source script collects the name and coverage of the quarters mentioned in the bulletin.
+#' 
+#' Both the source_automatic.R and source_manual.R scripts save the name and coverage of quarters mentioned in LAS bulletin.
+#' source_automatic.R retrieves the name of the latest CSV from the alpha-legal-aid-statistics-team AWS bucket. Based on the name of the file it creates the variables.
 #'
 #' @param auto Input TRUE if you want to run automatic source script or FALSE if you want to run manual source script.
 #
